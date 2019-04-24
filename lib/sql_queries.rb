01f0c706d3df8376bf4b6c47753c81d70def1157
Ming Duan <11203duan@gmail.com>
2019-04-24 22:58:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, SUM(pledges.amount)
+"SELECT users.name, pledges.amount
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
