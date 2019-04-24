a047feb8eb83f691c6ce8a71a8939ae78f00f40e
Ming Duan <11203duan@gmail.com>
2019-04-24 23:11:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY title
-WHERE amount >= funding_goal;"
+WHERE SUM(amount) >= funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
