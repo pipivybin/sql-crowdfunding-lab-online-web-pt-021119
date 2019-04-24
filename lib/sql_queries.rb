a44951237b3955b26f3b4b40ce397b051e64e74c
Ming Duan <11203duan@gmail.com>
2019-04-24 23:09:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT titles, amount - funding_goal
+"SELECT title, amount - funding_goal
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
