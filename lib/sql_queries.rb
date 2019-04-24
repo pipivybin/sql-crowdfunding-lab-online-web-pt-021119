cd24a03d416fa088e96f8217c6aca68060c33f13
Ming Duan <11203duan@gmail.com>
2019-04-24 23:18:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,7 +43,11 @@ ORDER BY SUM(amount), name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-""
+"SELECT 
+FROM
+INNER JOIN
+ON
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
