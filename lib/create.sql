383cbb3eb0e7d35a1fc55798694aa4e8d391e662
Ming Duan <11203duan@gmail.com>
2019-04-24 22:44:58 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -11,4 +11,11 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-)
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
